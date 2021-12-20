832b96ac52db461de43625f33f6e75cbbcca99a1
yasmimc <yasmimcavalcanti.r@gmail.com>
2021-12-20 15:15:54 -0300

    Add comment to indentify host and port if API is running locally

--- src/services/API/baseUrl.js
+++ src/services/API/baseUrl.js
@@ -1,6 +1,6 @@
 const API_URL =
     process.env.NODE_ENV === "production"
         ? "https://repo-provas-project.herokuapp.com/"
-        : "http://localhost:5000/";
+        : "http://localhost:5000/"; //set here the api url and port if you're running the backend locally
 
 export default API_URL;
